f45ebb69ca9d2d898d37686bc298ff4c13d19062
Kim Nguyen <design@kimisgold.com>
2020-03-17 14:51:08 -0400

    Remove markup insertion (now handled by js).

--- RRCHNMDonatePlugin.php
+++ RRCHNMDonatePlugin.php
@@ -4,19 +4,12 @@ class RRCHNMDonatePlugin extends Omeka_Plugin_AbstractPlugin
 {
     protected $_hooks = array (
         'public_head',
-        'public_body',
         );
 
     public function hookPublicHead()
     {
         queue_css_url('https://rrchnm.org/donate/donate-style.css');
-        queue_js_file('vendor/jquery');
         queue_js_url('https://rrchnm.org/donate/donate-positioning.js');
-    }
-
-    public function hookPublicBody() 
-    {
-        echo file_get_contents('https://rrchnm.org/donate/rrchnm-donate-banner.html');
     }
 }
 ?>
